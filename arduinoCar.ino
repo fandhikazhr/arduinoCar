cde74f8e28b264b394a22f02decaed11cc41f67a
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-17 20:26:10 +0700

    Update arduinoCar.ino

--- arduinoCar.ino
+++ arduinoCar.ino
@@ -5,3 +5,11 @@
 AF_DCMotor motor1(1, MOTOR12_1KHZ); 
 AF_DCMotor motor2(3, MOTOR12_1KHZ);
 int buzzer = 12;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  motor1.setSpeed(150); //speed motor 1-255
+  motor2.setSpeed(150); //speed motor 1-255
+}
