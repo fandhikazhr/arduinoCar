335d9a8f8e9dec85db7e24b2cbd846b3e088005f
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-17 20:27:32 +0700

    Update arduinoCar.ino

--- arduinoCar.ino
+++ arduinoCar.ino
@@ -16,6 +16,14 @@ void setup() {
 
 void loop() {
 
-
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+  Serial.println(distance);
 
 }
